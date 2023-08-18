288d67b655d94606aabea96d695719e3b41550de
tan <121097539@qq.com>
2023-08-18 16:10:30 +0800

    1

--- k8s/install/harbor/install.sh
+++ k8s/install/harbor/install.sh
@@ -1,3 +1,19 @@
+安装：
+https://zhuanlan.zhihu.com/p/638376379
+证书：
+https://zhuanlan.zhihu.com/p/234918875
+
+官网
 
 docker login localhost:8020 -u admin
 Harbor12345
+
+docker login -u admin -p Harbor12345  10.21.18.91:8020
+
+docker login -u admin -p Harbor12345  harbor91
+
+"insecure-registries": ["harbor91"],
+
+"tlscacert": "D:/Users/certs/ca.crt",
+  "tlscert": "D:/Users/certs/harbor91.cert",
+  "tlskey": "D:/Users/certs/harbor91.key"
