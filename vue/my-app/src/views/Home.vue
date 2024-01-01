caf76ade0a33d54d25a99b8f37649112e9e59371
tan <121097539@qq.com>
2024-01-02 00:28:30 +0800

    1

---
+++ vue/my-app/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>i am home!</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {}
+    }
+}
+</script>
---
+++ vue/my-app/src/views/Users.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>i am user!</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {}
+    }
+}
+</script>
